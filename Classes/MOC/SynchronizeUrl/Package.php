a12c2f977c868a26d9403b9b2c0f06132d13b8f4
Aske Ertmann <aertmann@users.noreply.github.com>
2016-03-28 17:26:06 +1300

    TASK: Compatibility with Neos 2.1.6+
    Uses the newly introduced ``TYPO3\Neos\Utility\NodeUriPathSegmentGenerator->generateUriPathSegment``
    method if available.

--- Classes/MOC/SynchronizeUrl/Package.php
+++ Classes/MOC/SynchronizeUrl/Package.php
@@ -17,7 +17,12 @@ class Package extends BasePackage {
 		$newUriPathSegment = NULL;
 		$dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodePropertyChanged', function(Node $node, $propertyName, $oldValue, $newValue) use($bootstrap, &$newUriPathSegment) {
 			if ($propertyName === 'title' && $node->getNodeType()->isOfType('TYPO3.Neos:Document')) {
-				$newUriPathSegment = strtolower(\TYPO3\TYPO3CR\Utility::renderValidNodeName($node->getProperty('title')));
+				if (method_exists('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator', 'generateUriPathSegment')) {
+					$nodeUriPathSegmentGenerator = $bootstrap->getObjectManager()->get('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator');
+					$newUriPathSegment = strtolower($nodeUriPathSegmentGenerator->generateUriPathSegment($node));
+				} else {
+					$newUriPathSegment = strtolower(\TYPO3\TYPO3CR\Utility::renderValidNodeName($node->getProperty('title') ?: $node->getName()));
+				}
 				$node->setProperty('uriPathSegment', $newUriPathSegment);
 				$bootstrap->getObjectManager()->get('TYPO3\Neos\Routing\Cache\RouteCacheFlusher')->registerNodeChange($node);
 			} elseif ($propertyName === 'uriPathSegment' && $newUriPathSegment !== NULL && $newValue !== $newUriPathSegment) {
