067379546e996d922839cd967ba660cb53459231
Elena Mukhina <pilotessa@gmail.com>
2016-07-14 18:17:57 +0300

    BUGFIX: URL path segments are not unique

--- Classes/MOC/SynchronizeUrl/Package.php
+++ Classes/MOC/SynchronizeUrl/Package.php
@@ -16,17 +16,27 @@ class Package extends BasePackage {
 		$dispatcher = $bootstrap->getSignalSlotDispatcher();
 		$newUriPathSegment = NULL;
 		$dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodePropertyChanged', function(Node $node, $propertyName, $oldValue, $newValue) use($bootstrap, &$newUriPathSegment) {
+			if (method_exists('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator', 'generateUriPathSegment') || method_exists('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator', 'setUniqueUriPathSegment')) {
+				$nodeUriPathSegmentGenerator = $bootstrap->getObjectManager()->get('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator');
+			}
 			if ($propertyName === 'title' && $node->getNodeType()->isOfType('TYPO3.Neos:Document')) {
 				if (method_exists('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator', 'generateUriPathSegment')) {
-					$nodeUriPathSegmentGenerator = $bootstrap->getObjectManager()->get('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator');
 					$newUriPathSegment = strtolower($nodeUriPathSegmentGenerator->generateUriPathSegment($node));
 				} else {
 					$newUriPathSegment = strtolower(\TYPO3\TYPO3CR\Utility::renderValidNodeName($node->getProperty('title') ?: $node->getName()));
 				}
-				$node->setProperty('uriPathSegment', $newUriPathSegment);
+				if (method_exists('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator', 'setUniqueUriPathSegment')) {
+					$nodeUriPathSegmentGenerator->setUniqueUriPathSegment($node);
+				} else {
+					$node->setProperty('uriPathSegment', $newUriPathSegment);
+				}
 				$bootstrap->getObjectManager()->get('TYPO3\Neos\Routing\Cache\RouteCacheFlusher')->registerNodeChange($node);
 			} elseif ($propertyName === 'uriPathSegment' && $newUriPathSegment !== NULL && $newValue !== $newUriPathSegment) {
-				$node->setProperty('uriPathSegment', $newUriPathSegment);
+				if (method_exists('TYPO3\Neos\Utility\NodeUriPathSegmentGenerator', 'setUniqueUriPathSegment')) {
+					$nodeUriPathSegmentGenerator->setUniqueUriPathSegment($node);
+				} else {
+					$node->setProperty('uriPathSegment', $newUriPathSegment);
+				}
 				$newUriPathSegment = NULL;
 			}
 		});
